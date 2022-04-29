ae614cd1aba71b24b105c8c07fd605decd34277c
hui <LFX525131>
2022-04-29 20:49:26 +0800

    查看

--- community-management/community-cloud/community-cloud-eureka/src/main/java/org/lufengxue/EurekaApplication.java
+++ community-management/community-cloud/community-cloud-eureka/src/main/java/org/lufengxue/EurekaApplication.java
@@ -15,7 +15,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 描    述:
  */
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
